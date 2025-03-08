a64209ddac9d6d2bada83d259fad68a17cf19115
yousef <yousefyasser948@gmail.com>
2025-03-08 23:57:18 +0200

    ..

--- lib/Widgets/home_view.dart
+++ lib/Widgets/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:bidayah/Screens/RoadMap_Screen.dart';
 import 'package:bidayah/Screens/Skill_selection_page.dart';
 import 'package:bidayah/Widgets/BottomNavBar%20.dart';
+import 'package:bidayah/Widgets/BuildIntoCard.dart';
 import 'package:bidayah/Widgets/Step_Progress.dart';
 import 'package:bidayah/Widgets/custom_home_appbar.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
@@ -57,14 +58,14 @@ class _HomeViewState extends State<HomeView> {
                     const SizedBox(height: 20),
                     _buildRoadmapCard(context),
                     const SizedBox(height: 30),
-                    _buildInfoCard(
+                    buildInfoCard(
                       icon: Icons.lightbulb,
                       color: Colors.orange,
                       title: "Tips & Information",
                       subtitle: "Learn about UI/UX design and interview techniques.",
                     ),
                     const SizedBox(height: 10),
-                    _buildInfoCard(
+                    buildInfoCard(
                       icon: Icons.description,
                       color: Colors.blue,
                       title: "CV Builder",
@@ -149,46 +150,5 @@ Widget _buildRoadmapCard(BuildContext context) {
   );
 }
 
-// ✅ Reusable Information Card
-Widget _buildInfoCard({
-  required IconData icon,
-  required Color color,
-  required String title,
-  required String subtitle,
-}) {
-  return Container(
-    margin: const EdgeInsets.only(bottom: 12),
-    padding: const EdgeInsets.all(16),
-    decoration: BoxDecoration(
-      color: Colors.white,
-      borderRadius: BorderRadius.circular(12),
-      boxShadow: [
-        BoxShadow(color: Colors.black12, blurRadius: 6, spreadRadius: 2),
-      ],
-    ),
-    child: Row(
-      children: [
-        CircleAvatar(
-          backgroundColor: color.withOpacity(0.2),
-          child: Icon(icon, color: color),
-        ),
-        const SizedBox(width: 12),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                title,
-                style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              Text(
-                subtitle,
-                style: const TextStyle(fontSize: 14, color: Colors.grey),
-              ),
-            ],
-          ),
-        ),
-      ],
-    ),
-  );
-}
+
+
